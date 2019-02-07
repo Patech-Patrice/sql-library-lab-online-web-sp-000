0177e9aba7c8d70893a74fceb85d62d65684eb0d
Patrice Drayton <draytonpatrice@marilynssecret.org>
2019-02-07 19:35:32 +0000

    finished creating schema.sql and insert.sql

--- lib/schema.sql
+++ lib/schema.sql
@@ -4,3 +4,35 @@ create table series(
   author_id INTEGER,
   subgenre_id INTEGER
 );
+
+create table subgenres(
+  id INTEGER primary key,
+  name TEXT
+);
+
+ create table authors(
+  id INTEGER primary key,
+  name TEXT
+);
+
+ create table books(
+  id INTEGER primary key,
+  title TEXT,
+  year INTEGER,
+  series_id INTEGER
+);
+
+ create table characters(
+  id INTEGER primary key,
+  name TEXT,
+  motto TEXT,
+  species TEXT,
+  author_id INTEGER,
+  series_id INTEGER
+);
+
+ create table character_books(
+  id INTEGER primary key,
+  book_id INTEGER,
+  character_id INTEGER
+)
