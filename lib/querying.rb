7cca48ba195cfdd7fa14a578c164f94308b8d59e
Patrice Drayton <draytonpatrice@marilynssecret.org>
2019-02-07 19:48:57 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,5 +1,8 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "select books.title, books.year from books where books.series_id = 1 order by books.year ASC;"
+  "SELECT books.title, books.year 
+    FROM books JOIN series ON series.id = books.series_id       
+    WHERE series.id =1
+    GROUP BY books.year;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
