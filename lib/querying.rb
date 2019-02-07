51f6dfa0fc0f2ee8a2aaad043e6734ce625a38b2
Patrice Drayton <draytonpatrice@marilynssecret.org>
2019-02-07 19:37:28 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -16,9 +16,9 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "select series.title from characters inner join series on characters.series_id = series.id group by series.title having characters.species = 'human' order by count(characters.species) DESC limit 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "select characters.name, count(books.title) from character_books inner join characters on character_books.character_id = characters.id inner join books on character_books.book_id = books.id group by characters.name order by count(books.title) DESC;""
 end
